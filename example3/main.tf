e9c080d3dd1b01f68c4f21723a6cc3c9399158e5
Jason Renaud <renaudjj@g.cofc.edu>
2019-01-23 09:36:53 -0700

    Trying to create an Organization but getting AccessDenied. Need to reasearch IAM/SCP to make this work

--- example3/main.tf
+++ example3/main.tf
@@ -1,3 +1,11 @@
 provider "aws" {
   region = "us-west-2"
 }
+
+resource "aws_organizations_organization" "org" {
+  aws_service_access_principals = [
+    "cloudtrail.amazonaws.com",
+    "config.amazonaws.com"
+  ]
+  feature_set = "ALL"
+}
